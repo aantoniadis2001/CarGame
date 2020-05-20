fa39dffc522041dc61db53f1b81f3536186ca881
Vaggelis <ebarmpas>
2020-05-20 19:04:42 +0300

    Added assets and updated GUI functionalities

--- src/Grid.java
+++ src/Grid.java
@@ -1,28 +1,28 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
 
 public class Grid {
-
+	final static int X = 1;
+	final static int Y = 0;
+	
 	private int[][] grid;
 	private int[] gridDimension = new int[2];
 	private int[] percentage = new int[3];
 	
 	public Grid(int[] dim, int[] perc) {
 		int rng;
-		Integer choice;
+		int choice;
 		Random rand = new Random();
 		gridDimension = dim;
 		percentage = perc;
 		
-		grid = new int[gridDimension[0]][gridDimension[1]];
+		grid = new int[gridDimension[X]][gridDimension[Y]];
 		
-		for(int i = 0; i < (gridDimension[0]); i++){
-			for(int j = 0; j < gridDimension[1]; j++) {
+		for(int i = 0; i < (gridDimension[X]); i++){
+			for(int j = 0; j < gridDimension[Y]; j++) {
 				rng = rand.nextInt(101);
 				if(rng < percentage[0] || (i == 0 && j == 0) || (i == gridDimension[0] - 1 && j == gridDimension[1] - 1)) {
 					choice = 0;
-				}
+				} 
 				else if(rng < percentage[0] + percentage[1]) {
 					choice = 1;
 				}
