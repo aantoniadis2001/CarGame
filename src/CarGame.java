b29b19601fbb642be416a2ed9a2b7865114c5a84
aantoniadis <aantoniadis@citycollege.sheffield.eu>
2020-05-20 18:59:45 +0300

    Turns function added, removed some unnecessary libraries from Grid,GridIni...

--- src/CarGame.java
+++ src/CarGame.java
@@ -2,22 +2,26 @@ import java.util.concurrent.TimeUnit;
 
 public class CarGame
 {	
+	private static Car[] car;
 	private static int diceRoll;
+	private static int[] pOutOfFuelTurns = {0,0};
+	private static int choice;
+	private static int[] maxDim;
+	private static int borderCounter;
+	private static int cellType;
+	private static GreyTile grey = new GreyTile();
+	private static GreenTile green = new GreenTile();
+	private static BlackTile black = new BlackTile();
+	private static Grid grid;
+	private static NoFuelGui nfg;
 	
 	public static void main(String[] args) 
 	{	
 		int turnCounter = 1;
-		int p1OutOfFuelTurns = 0;
-		int p2OutOfFuelTurns = 0;
 		
-		Car []car = new Car[2];
+		car = new Car[2];
 		car[0] = new Car();
 		car[1] = new Car();
-		
-		int[] p1Position = car[0].getPosition();
-		int[] p2Position = car[1].getPosition();
-		
-		
 		
 		GridInitializationGui gig = new GridInitializationGui("Set game parameters");
 		gig.setVisible(true);
@@ -34,88 +38,98 @@ public class CarGame
 			}
 		}
 		
-		Grid grid = new Grid(gig.getDim(), gig.getPerc());				
-		MainFrame frame = new MainFrame("Car game", grid);
+		grid = new Grid(gig.getDim(), gig.getPerc());				
+		MainFrame frame = new MainFrame("Car Game", grid);
 		frame.setVisible(true);
 		
-		NoFuelGui nfg = new NoFuelGui(" yes");
+		nfg = new NoFuelGui("You are out of fuel!");
 		nfg.setVisible(false);
 		
-		int[] maxDim = grid.getDimensions();
-		int borderCounter;
-		int cellType;
+		maxDim = grid.getDimensions();
+		maxDim[0]++;
+		maxDim[1]++;
 		
-		GreyTile grey = new GreyTile();
-		GreenTile green = new GreenTile();
-		BlackTile black = new BlackTile();
-		int choice;
-		
-		while((car[0].getPosition() == maxDim) && (car[1].getPosition() == maxDim))
+		while((car[0].getPosition() != maxDim) && (car[1].getPosition() != maxDim))
 		{
 			if(turnCounter % 2 != 0)
 			{
-				cellType = grid.getCell(car[0].getPosition(0), car[0].getPosition(1));
-				
-				if(car[0].getFuel() == 0)
+				turns(0);
+			}
+			else
+			{
+				turns(1);
+			}
+			turnCounter++;
+		}
+	}
+	
+	public static void turns(int p)
+	{
+		cellType = grid.getCell(car[p].getPosition(0), car[p].getPosition(1));
+		
+		if(car[p].getFuel() == 0)
+		{
+			nfg.setVisible(true);
+			while(nfg.isVisible())
+			{
+				try 
 				{
-					nfg.setVisible(true);
-					while(nfg.isVisible())
-					{
-						try 
-						{
-							TimeUnit.SECONDS.sleep(1);
-						} 
-						catch (InterruptedException e) 
-						{
-							e.printStackTrace();
-						}
-					}
-					choice = nfg.getRounds();
-					if(choice == 0)
-					{
-						car[0].setPosition(0, 0);
-						car[0].setFuel(20 + (int)(Math.random() * ((120 - 20) + 20)));
-					}
-					else
-					{
-						p1OutOfFuelTurns = choice;
-						car[0].setFuel(choice * 20);
-					}
+					TimeUnit.SECONDS.sleep(1);
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
 				}
+			}
+			choice = nfg.getRounds();
+			if(choice == 0)
+			{
+				car[p].setPosition(0, 0);
+				car[p].setFuel(20 + (int)(Math.random() * ((120 - 20) + 20)));
+			}
+			else
+			{
+				pOutOfFuelTurns[p] = choice;
+				car[p].setFuel(choice * 20);
+			}
+		}
+		else
+		{
 				
-				if (p1OutOfFuelTurns != 0)
+			if (pOutOfFuelTurns[p] != 0)
+			{
+				pOutOfFuelTurns[p]--;
+			}
+			else
+			{
+				rollDice();
+				borderCounter = maxDim[0] - (car[p].getPosition(0) + diceRoll);
+				if(borderCounter >= 0 )
 				{
-					p1OutOfFuelTurns--;
-					break;
+					car[p].setPositionX(car[p].getPosition(0) + diceRoll);
 				}
 				else
 				{
-					rollDice();
-					borderCounter = maxDim[1] - (car[0].getPosition(1) + diceRoll);
-					if(borderCounter >= 0 )
-					{
-						car[0].setPositionX(car[0].getPosition(0) + diceRoll);
-					}
-					else
-					{
-						car[0].setPosition(maxDim[0] + borderCounter, car[0].getPosition(1) + 1);
-						
-					}
-				}
-				cellType = grid.getCell(car[0].getPosition(0), car[0].getPosition(1));
-				switch (cellType)
-				{
-					case 0:
-						grey.doEffect(car[0]);
-					case 1:
-						green.doEffect(car[0]);
-					case 2:
-						black.doEffect(car[0]);
+					car[p].setPosition(maxDim[0] + borderCounter, car[p].getPosition(1) + 1);
+					
 				}
 			}
-			
+			cellType = grid.getCell(car[p].getPosition(0), car[p].getPosition(1));
+			switch (cellType)
+			{
+				case 0:
+					grey.doEffect(car[p]);
+					break;
+				case 1:
+					green.doEffect(car[p]);
+					break;
+				case 2:
+					black.doEffect(car[p]);
+					break;
+			}
 		}
 	}
+	
 	public static void rollDice()
 	{
 		diceRoll = 1 + (int)(Math.random() * ((6 - 1) + 1));
--- src/GridInitializationGui.java
+++ src/GridInitializationGui.java
@@ -1,9 +1,7 @@
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.Visibility;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,8 +9,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
-import com.sun.corba.se.spi.ior.MakeImmutable;
 
 public class GridInitializationGui extends JFrame{
 	
